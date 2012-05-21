5232c6c68141e1719aa0f32d9a675e44e677284a
Tim Neems <tneems@gmail.com>
2012-05-21 16:31:47 -0400

    Remove unused preference

--- app/models/spree/sitemap_configuration.rb
+++ app/models/spree/sitemap_configuration.rb
@@ -1,6 +1,5 @@
 module Spree
   class SitemapConfiguration < Preferences::Configuration
-    preference :url,                        :string, :default => "http://spree.store/"
     preference :include_product_groups,     :boolean, :default => true
     preference :include_product_taxonomies, :boolean, :default => true
   end
