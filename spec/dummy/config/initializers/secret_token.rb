ec5cff2227fc97639bc714191206b4bbd23da34e
Tim Neems <tneems@gmail.com>
2012-05-21 15:45:20 -0400

    Remove dummy app in favor of generated app

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '184eafac74a5def58257f36f3e78101fa1b211b6ec627cac876c7ab1134c2213fcfa81d5c6a61f5b603d7cdadcd9890c1d7124fadefd91b022a6dff8acb0202d'
